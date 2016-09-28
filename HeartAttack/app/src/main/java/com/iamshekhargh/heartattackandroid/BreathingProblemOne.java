368aed97ddaf1fefc4c185d8874351033cbd8ff3
iamShekharGH <shekhargh@shekhargh.com>
2016-09-28 07:26:46 +0530

    Made 2 pages

---
+++ HeartAttack/app/src/main/java/com/iamshekhargh/heartattackandroid/BreathingProblemOne.java
@@ -0,0 +1,15 @@
+package com.iamshekhargh.heartattackandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BreathingProblemOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breathing_problem_one);
+
+
+    }
+}
---
+++ HeartAttack/app/src/main/java/com/iamshekhargh/heartattackandroid/WhatIsYourAge.java
@@ -0,0 +1,13 @@
+package com.iamshekhargh.heartattackandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhatIsYourAge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_is_your_age);
+    }
+}
